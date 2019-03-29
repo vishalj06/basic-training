49c1993276a38271d7c1bc7c94eb02ac0e3ee788
vishaltechy <48709800+vishaltechy@users.noreply.github.com>
2019-03-29 17:56:20 +0530

    Update FirstAss.js

--- FirstAss.js
+++ FirstAss.js
@@ -1,102 +1,85 @@
-/ Problem 1: Complete the secondLargest function which takes in an array of numbers in input and return the second biggest number in the array. (without using sort)?
+/// Problem 1: Complete the secondLargest function which takes in an array of numbers in input and return the second biggest number in the array. (without using sort)?
 function secondLargest(array) {
- // Write your code here
- let largest=0,secondL=0;
- let i=array.length;
- for(let m=0;m<i;m++){
- 
-   if(largest < array[m])
-     {
-       secondL = largest;
-       largest = array[m];
-     }
-   else
-     {
-       if(secondL < array[m])
-       {
-         secondL=array[m];
-       }
-     }
- }
-
- return secondL;
+  // Write your code here
+ var fl=0,sl=0;
+ for(no of array){
+   if(no>fl){
+     sl=fl
+     fl=no
+   }
+   else if(no>sl)
+     sl=no
+ } 
+  return sl
 }
 
 // Problem 2: Complete the calculateFrequency function that takes lowercase string as input and returns frequency of all english alphabet. (using only array, no in-built function)
 function calculateFrequency(string) {
- // Write your code here
- let myarray = {};
- let i = string.length;
- for(let m=0;m<i;m++)
-   {
-     if(string[m] >= "a" && string[m] <= "z")
-       {
-         if(string[m] in myarray)
-           {
-             myarray[string[m]] +=1;
-           }
-         else myarray[string[m]] = 1;
-       }
-   }
- return myarray;
+  // Write your code here
+  var obj={}
+  for(var char1 of string)
+    { if(char1 >='a' && char1<='z'){
+      if(char1 in obj)
+      obj[char1]+=1
+      else
+        obj[char1]=1
+
+     }
+    }
+  return obj
 }
 
 // Problem 3: Complete the flatten function that takes a JS Object, returns a JS Object in flatten format (compressed)
 function flatten(unflatObject) {
- // Write your code here
-   var ans = {};
-   function callMeBack(data, key) {
+ 
+   var obj = {};
+   function flattening(data, key) {
        if (Array.isArray(data)) {
-         // console.log(Object(data) + "    " + data + "   "+ key + "In array");
-           //console.log("Array found : "+ data + Array.isArray(data));
-           let l = data.length;
-           for (let i = 0 ; i < l; i++)
-           callMeBack(data[i], key + "." + i);
+           
+           for (let i in data)
+           flattening(data[i], key + "." + i);
        }
        else {
            if (Object(data) === data) {
-             //console.log(Object(data) + "    " + data + "   "+ key);
+
                for (i in data) {
                  if(key)
-                   callMeBack(data[i], key + "." + i);
-                 else callMeBack(data[i],i);
+                   flattening(data[i], key + "." + i);
+                 else flattening(data[i],i);
                }
            }
            else{
-               ans[key] = data;
+               obj[key] = data;
              }
        }
    }
-   callMeBack(unflatObject, "");
-   return ans;
+   flattening(unflatObject, "");
+   return obj;
 }
 
 // Problem 4: Complete the unflatten function that takes a JS Object, returns a JS Object in unflatten format
+
 function unflatten(flatObject) {
  // Write your code here
- var result = {}, temp, keys, data, i;
- function getKeys(data)
- {
-   var arr = [];
-   arr = (data.toString()).split(".");
-   return arr;
- }
- for (data in flatObject) {
-     keys = getKeys(data);
-     let l = keys.length;
-     temp = result;
-     for (i = 0; i < l - 1; i++) {
-         if (!(keys[i] in temp)) {
-             if (isFinite(keys[i + 1])) {
-                 temp[keys[i]] = [];
+var fobj = {}
+var curr, keys, data, ind; 
+for (data in flatObject) 
+{
+        keys = (data.toString()).split(".");
+        let l = keys.length;
+	curr = fobj;     
+	for (ind = 0; ind < l - 1; ind++) 
+	{
+             if (!(keys[ind] in curr)) {
+             if (isFinite(keys[ind + 1])) {
+                 curr[keys[ind]] = [];
              }
              else
-                 temp[keys[i]] = {};
-
+                 curr[keys[ind]] = {};			
          }
-         temp = temp[keys[i;
+         curr = curr[keys[ind]];	
      }
-     temp[keys[l - 1]] = flatObject[data];
+     curr[keys[l - 1]] = flatObject[data];
  }
- return result;
+ return fobj;
 }
